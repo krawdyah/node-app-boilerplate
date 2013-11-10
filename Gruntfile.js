f455226d68d97b56d3d9584ad5157de510cb1537
Claudia Farias <krawdyah@gmail.com>
2013-11-10 20:39:56 -0200

    Improve gruntfile; add compass, deploy and new server tasks :beer: :metal: :dancer:

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,17 @@
 var request = require('request');
 
 module.exports = function (grunt) {
-  require('time-grunt')(grunt);
-  require('load-grunt-tasks')(grunt);
-
-  var reloadPort = 35729, files;
-
-  grunt.loadNpmTasks('grunt-contrib-coffee');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    develop: {
-      server: {
-        file: 'server.js'
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'public/stylesheets/sass',
+          cssDir: 'public/stylesheets',
+          outputStyle: 'compressed',
+          watch: true
+        }
       }
     },
     coffee: {
@@ -32,39 +30,72 @@ module.exports = function (grunt) {
         }
       }
     },
-    watch: {
+    jshint: {
+      files: ['app/**/*.js', 'config/*.js', 'public/javascripts/*.js'],
       options: {
-        nospawn: true,
-        livereload: reloadPort
-      },
+        asi: true,
+        curly: true,
+        eqeqeq: true,
+        eqnull: true,
+        browser: true,
+        laxcomma: true,
+        laxbreak: true
+      }
+    },
+    watch: {
       js: {
-        files: [
-          'app.js',
-          'app/**/*.js',
-          'config/*.js'
-        ],
-        tasks: ['develop', 'delayed-livereload']
+        files: ['<%= jshint.files %>'],
+        tasks: ['server', 'jshint', 'coffee']
+      },
+      compass: {
+        files: ['public/stylesheets/sass/**/*.sass'],
+        tasks: ['compass']
+      },
+      options: {
+        livereload: true
       }
     }
   });
 
-  grunt.config.requires('watch.js.files');
-  files = grunt.config('watch.js.files');
-  files = grunt.file.expand(files);
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function () {
-    var done = this.async();
-    setTimeout(function () {
-      request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','),  function(err, res) {
-          var reloaded = !err && res.statusCode === 200;
-          if (reloaded)
-            grunt.log.ok('Delayed live reload successful.');
-          else
-            grunt.log.error('Unable to make a delayed live reload.');
-          done(reloaded);
-        });
-    }, 500);
+  grunt.event.on('server', function(done) {
+    grunt.child = grunt.util.spawn({ 
+        cmd: 'node', args: ['server']
+      }, function (error, result){
+        grunt.log.error(result.stderr).writeln()
+    }).stdout.on('data', function(buffer) {
+      grunt.log.write('\r[node] > '.cyan + String(buffer));
+    });
+    setTimeout(function() {
+      grunt.log.writeln('Wait! Starting server...');
+      done();
+    }, 1000);
   });
-  
-  grunt.registerTask('default', ['develop', 'watch']);
+
+  grunt.event.on('deploy', function(done) {
+    grunt.child = grunt.util.spawn({ 
+        cmd: 'git', args: ['push', 'heroku', 'master']
+      }, function (error, result){
+        grunt.log.write('\r\n\r\n');
+        grunt.log.error(result.stderr).writeln()
+    }).stdout.on('data', function(buffer) {
+      grunt.log.write('\r\n[deploy] > '.cyan + String(buffer));
+    });
+    grunt.log.write('\r\n[deploy] > '.cyan + 'Starting deploy...');
+  });
+
+  grunt.registerTask('deploy', 'Deploy to heroku', function(){
+    grunt.event.emit('deploy', this.async());
+  });
+
+  grunt.registerTask('server', 'Starts node server', function() {
+    grunt.event.emit('server', this.async());
+  });
+
+  grunt.registerTask('default', ['server', 'jshint', 'coffee', 'compass', 'uglify', 'watch']);
 };
